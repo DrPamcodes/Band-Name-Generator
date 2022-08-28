19f9023665df9e53d3113aa83fae17938ace75fe
Pamela Eteng <51480058+DrPamcodes@users.noreply.github.com>
2022-08-28 22:20:51 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator.")
+
+city_name = input("What's the name of the city you grew up in?\n")
+
+pet_name = input("What's the name of your pet?\n")
+
+user_name = city_name + "_" + pet_name
+
+print("Your new username is " + user_name)
